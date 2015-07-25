9d324115b9577a570d1b41c5c2b97241d9ba04e2
JorgeLuis <JorgeLuis@192.168.10.37>
2015-07-24 19:14:15 -0500

    tarea

---
+++ public_html/js/app.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//Primero creamos un modulo, el cual es la aplicación de angular JS.
+var aplicacion=angular.module('aplicacionsita',[]);
+//En los corchetes van las directivas.
+
+//Definimos un controlador para comunicarnos con el index.html
+aplicacion.controller('controladorPrincipal',function ($scope){
+    //Creamos un modelo simple.
+    //scope.- alcance
+    $scope.mensajito='Hola desde angular!!!';
+    $scope.nombre='';
+    $scope.algo='';
+    $scope.gCentigrados='';
+    $scope.convertir='';
+    $scope.centi= function (){
+      $scope.convertir= (gCentigrados*1.8)+32,'resultado';
+    };
+    
+    $scope.apachurrame= function(){
+        $scope.algo='haz apachurrado el boton';
+    };
+}
+        ); 
